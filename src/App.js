a620aa473a4207450a32982ab47f2870172074b3
ESSALHIN <nadiasalhi@hotmail.fr>
2022-05-13 21:21:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from './image/image.jpg';
+import myvideo from './video/myVideo.mp4'
+import { findByLabelText } from '@testing-library/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<div style={{border:'solid 1px black',maxwidth:"100vw"}}>
+
+ <h1 style={{textAlign:"center",color:"red"}}className="title red">Nadia ESSALHI</h1>
+
+ <br/>
+
+ <img src={"./image/image.jpg"} alt="image" style={{diplay:"flex", justifyContent:"center", margin:"25%"}} />
+
+ <br/>
+ 
+ <img src={"/public/img.jpg"} alt='img' style={{diplay:"flex", justifyContent:"center", margin:"25%"}}/>
+ <br/>
+ 
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type={"video/mp4"} style={{diplay:"flex", justifyContent:"center", margin:"25%", width:"auto", height:"auto"}} />
+
+</video>
+</>
+
+
   );
 }
 
